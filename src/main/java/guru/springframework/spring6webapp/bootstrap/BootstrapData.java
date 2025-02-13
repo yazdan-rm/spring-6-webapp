945d56c6a6ec1aebe9f6f9e7ca61faa7fc0f0f15
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-13 16:44:58 +0330

    Feature: Add repository for book and author

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
